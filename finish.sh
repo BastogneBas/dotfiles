5e6ee21f1cd90387447814e50b6c76452fa42584
BastogneBas <11506433+BastogneBas@users.noreply.github.com>
2019-11-17 10:55:00 +0100

    Add preparation files

---
+++ finish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo umount vim
+sudo umount xkb
+
+sudo umount Backgrounds
+
+sudo umount config/fontconfig
+sudo umount config/gtk-2.0
+sudo umount config/gtk-3.0
+sudo umount config/gtk-4.0
+sudo umount config/i3
+sudo umount config/i3status
+sudo umount config/mpd
+sudo umount config/mpDris2
+sudo umount config/neofetch
+sudo umount config/nitrogen
+sudo umount config/polybar
+sudo umount config/rofi
+sudo umount config/sway
+sudo umount config/swaylock
+sudo umount config/termite
+sudo umount config/waybar
+
+sudo umount etc/X11
---
+++ prepare.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cp ~/.profile profile
+cp ~/.vimrc vimrc
+cp ~/.xinitrc xinitrc
+cp ~/.Waylandresources Waylandresources
+cp ~/.Xresources Xresources
+cp ~/.zshrc zshrc
+sudo mount --bind ~/.vim vim
+sudo mount --bind ~/.xkb xkb
+
+sudo mount --bind ~/Backgrounds Backgrounds
+
+sudo mount --bind ~/.config/fontconfig config/fontconfig
+sudo mount --bind ~/.config/gtk-2.0 config/gtk-2.0
+sudo mount --bind ~/.config/gtk-3.0 config/gtk-3.0
+sudo mount --bind ~/.config/gtk-4.0 config/gtk-4.0
+sudo mount --bind ~/.config/i3 config/i3
+sudo mount --bind ~/.config/i3status config/i3status
+sudo mount --bind ~/.config/mpd config/mpd
+sudo mount --bind ~/.config/mpDris2 config/mpDris2
+sudo mount --bind ~/.config/neofetch config/neofetch
+sudo mount --bind ~/.config/nitrogen config/nitrogen
+sudo mount --bind ~/.config/polybar config/polybar
+sudo mount --bind ~/.config/rofi config/rofi
+sudo mount --bind ~/.config/sway config/sway
+sudo mount --bind ~/.config/swaylock config/swaylock
+sudo mount --bind ~/.config/termite config/termite
+sudo mount --bind ~/.config/waybar config/waybar
+
+sudo mount --bind /etc/X11/ etc/X11
